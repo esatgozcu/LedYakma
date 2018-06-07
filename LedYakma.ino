dc3a543e03b24c16c53238daef81648a7a3cdc4d
esatgozcu <35576161+esatgozcu@users.noreply.github.com>
2018-06-07 05:47:42 +0300

    Add files via upload

---
+++ LedYakma.ino
@@ -0,0 +1,12 @@
+void setup() {
+
+  pinMode(13,OUTPUT);       // 13 Numaralı pin çıkış olarak ayarlandı.
+}
+
+void loop() {
+  
+  digitalWrite(13,HIGH);    // Ledi yak.
+  delay(1000);              // 1 saniye bekle.
+  digitalWrite(13,LOW);     // Ledi söndür.
+  delay(1000);              // 1 saniye bekle.
+}
